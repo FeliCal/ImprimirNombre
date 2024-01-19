46148b1d217221d13025f0832f5f98efbdcabc4e
FeliCal <felical2005@gmail.com>
2024-01-18 20:32:43 -0600

    Imprimir nombre

---
+++ src/ImprimirNombre.java
@@ -0,0 +1,10 @@
+public class ImprimirNombre {
+
+    public static void main(String[] args) {
+
+        String saludo = "Hola, mi nombre es";
+        String nombre = "Felipe";
+
+        System.out.println(saludo + " " + nombre);
+    }
+}
